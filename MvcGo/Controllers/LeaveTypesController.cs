cc15c509904e3667f95da5ee2bc41ca7587b6d9c
Vladhus <49909976+Vladhus@users.noreply.github.com>
2020-10-11 17:23:18 +0200

    TDP-3 Added "Delete record functionality"

--- MvcGo/Controllers/LeaveTypesController.cs
+++ MvcGo/Controllers/LeaveTypesController.cs
@@ -32,7 +32,13 @@ namespace MvcGo.Controllers
         // GET: LeaveTypesController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            if (!_repo.isExists(id))
+            {
+                return NotFound();
+            }
+            var leaveType = _repo.FindById(id);
+            var model = _mapper.Map<LeaveTypeVM>(leaveType);
+            return View(model);
         }
 
         // GET: LeaveTypesController/Create
@@ -113,21 +119,41 @@ namespace MvcGo.Controllers
         // GET: LeaveTypesController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var leaveType = _repo.FindById(id);
+            if (leaveType == null)
+            {
+                return NotFound();
+            }
+            var isSuccess = _repo.Delete(leaveType);
+            if (!isSuccess)
+            {
+                return BadRequest();
+            }
+            return RedirectToAction(nameof(Index));
         }
 
         // POST: LeaveTypesController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id,LeaveTypeVM model)
         {
             try
             {
+                var leaveType = _repo.FindById(id);
+                if (leaveType == null)
+                {
+                    return NotFound();
+                }
+                var isSuccess = _repo.Delete(leaveType);
+                if (!isSuccess)
+                {
+                    return View(model);
+                }
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(model);
             }
         }
     }
